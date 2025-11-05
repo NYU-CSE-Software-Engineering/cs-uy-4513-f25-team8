273cafa171d2422b59b09fc6c1ae32435bdc5612
Jing Qian <jqian2272@gmail.com>
2025-11-05 04:56:45 -0500

    configure database cleaner to clean db between cucumber tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -51,3 +51,15 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
+
+
+# before and after hooks for db_cleaner
+# start db_cleaner before each test
+Before do
+  DatabaseCleaner.start
+end
+
+# clean db after each test
+After do
+  DatabaseCleaner.clean
+end
