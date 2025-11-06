b130b39ab3ce2689764cfef4f419434c195e0d0b
lcm9995 <lcm9995@nyu.edu>
2025-11-06 18:23:32 -0500

    Add failing model spec for user role validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     it "requires username" do
-    u = User.new(username: nil)
-    expect(u).not_to be_valid
-    expect(u.errors[:username]).to be_present
-  end
+        u = User.new(username: nil)
+        expect(u).not_to be_valid
+        expect(u.errors[:username]).to be_present
+    end
+    it "requires role" do
+        u = User.new(username: "lily", role: nil)
+        expect(u).not_to be_valid
+        expect(u.errors[:role]).to be_present
+    end
 end
+
