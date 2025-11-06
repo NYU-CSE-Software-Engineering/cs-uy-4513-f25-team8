e1b0d2da67d387e01da7c1ac0c33c63a3714d105
lcm9995 <lcm9995@nyu.edu>
2025-11-06 18:54:18 -0500

    Make user default account status validation spec pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     ROLES = %w[renter owner admin].freeze
     validates :username, presence: true
     validates :role, presence: true, inclusion: { in: ROLES }
+    attribute :account_status, :string, default: "active"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_06_232349) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_06_234301) do
   create_table "users", force: :cascade do |t|
+    t.string "account_status"
     t.datetime "created_at", null: false
     t.string "role"
     t.datetime "updated_at", null: false
