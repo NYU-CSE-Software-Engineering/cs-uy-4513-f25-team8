a84a64010cc6cefa750b4cdec7e2d5ccb2ceb4ac
lcm9995 <lcm9995@nyu.edu>
2025-11-06 19:08:50 -0500

    Add failing item spec for item owner validation

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -19,4 +19,9 @@ RSpec.describe Item, type: :model do
         i = Item.create!(title: "Tripod", price: 25.00)
         expect(i.availability_status).to eq("available")
     end
+    it "belongs to an owner (User)" do
+        owner = User.create!(username: "owner1", role: "owner")
+        item  = Item.create!(title: "Camera", price: 25.0, owner: owner)
+        expect(item.owner).to eq(owner)
+    end
 end
