6699de5a85f4b200df628b899df6518126fd36d2
lcm9995 <lcm9995@nyu.edu>
2025-11-06 20:40:51 -0500

    Make booking spec approving transitions pass

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,14 +2,17 @@ class Booking < ApplicationRecord
   belongs_to :item
   belongs_to :renter, class_name: "User", optional: true
   belongs_to :owner, class_name: "User", optional: true
+
   enum :status, { requested: 0, approved: 1 }
   before_validation :set_default_status, on: :create
+
+  validates :status, presence: true, inclusion: { in: statuses.keys }
   validate :start_date_before_end_date
   validate :item_must_be_available, on: :create
   validate :cannot_approve_unavailable_item
   private
   def set_default_status
-    self.status = :requested if status.blank?
+    self.status ||= :requested
   end
   def start_date_before_end_date
     if start_date.present? && end_date.present? && start_date > end_date
