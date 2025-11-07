1b8e9c4605c848dc86090d5ecc5f1ed388ba1701
lcm9995 <lcm9995@nyu.edu>
2025-11-06 19:40:28 -0500

    Added failing booking spec for requires a renter

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe Booking, type: :model do
         expect(b).not_to be_valid
         expect(b.errors[:item]).to be_present
     end
+    it "belongs to a renter (User)" do
+        renter = User.create!(username: "isabelle", role: "renter")
+        owner  = User.create!(username: "erfu", role: "owner")
+        item   = Item.create!(title: "Camera", price: 25.0, owner: owner)
+
+        b = Booking.create!(
+            item: item,
+            renter: renter,
+            owner: owner,
+            start_date: Date.new(2025, 3, 21),
+            end_date: Date.new(2025, 3, 22)
+        )
+
+        expect(b.renter).to eq(renter)
+        expect(b.renter).to be_a(User)
+    end
 end
