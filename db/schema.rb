d26a7d3a71362cf206e86c8a14ff0f8af5605b73
lcm9995 <lcm9995@nyu.edu>
2025-11-06 19:01:48 -0500

    Make item title validation spec pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_06_235624) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_07_000033) do
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "account_status"
     t.datetime "created_at", null: false
