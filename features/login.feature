8ade42cc9857183c8ae26dc134a6c0b4aa448d15
Jing Qian <jqian2272@gmail.com>
2025-11-07 16:17:15 -0500

    fix ambiguous match on input fields

--- features/login.feature
+++ features/login.feature
@@ -1,8 +1,8 @@
 Feature: User Login
   Background:
     Given I am on the login page
-    And I fill in the login field "Email" with "kyle.jia@nyu.edu"
-    And I fill in the login field "Password" with "Team8isTheBest123!"
+    And I fill in the login field "email" with "kyle.jia@nyu.edu"
+    And I fill in the login field "password" with "Team8isTheBest123!"
 
   Scenario: Successful login
     When I press "Log in"
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,11 +1,23 @@
+Given("I am on the login page") do
+  visit "/login"
+end
+
 Given("I fill in the login field {string} with {string}") do |field, value|
   fill_in field, with: value
 end
 
-Then('I should see the {string} link') do |link_text|
+When("I press {string}") do |button|
+  click_button button
+end
+
+Then("I should see {string}") do |text|
+  expect(page).to have_content(text)
+end
+
+Then("I should see the {string} link") do |link_text|
   expect(page).to have_link(link_text)
 end
 
-Then('the page title should be {string}') do |title|
+Then("the page title should be {string}") do |title|
   expect(page).to have_title(title)
 end
