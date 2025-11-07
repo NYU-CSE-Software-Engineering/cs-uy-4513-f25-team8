e77933c86870f680d6a001eb5e12c4d32457a7f7
lcm9995 <lcm9995@nyu.edu>
2025-11-06 19:03:00 -0500

    Add failing item spec for item price validation

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
     it "requires title" do
-    i = Item.new(title: nil)
-    expect(i).not_to be_valid
-    expect(i.errors[:title]).to be_present
-  end
+        i = Item.new(title: nil)
+        expect(i).not_to be_valid
+        expect(i.errors[:title]).to be_present
+    end
+    it "requires a positive price" do
+        i = Item.new(title: "Camera", price: nil)
+        expect(i).not_to be_valid
+        expect(i.errors[:price]).to be_present
+
+        i.price = -5
+        expect(i).not_to be_valid
+        expect(i.errors[:price]).to be_present
+    end
 end
