10e9ac9d4148102f48d1ac964dca65d4752d7151
lcm9995 <lcm9995@nyu.edu>
2025-11-06 19:57:49 -0500

    Make booking date range validation spec pass

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,8 +4,14 @@ class Booking < ApplicationRecord
   belongs_to :owner, class_name: "User", optional: true
   enum :status, { requested: 0, approved: 1 }
   before_validation :set_default_status, on: :create
+  validate :start_date_before_end_date
   private
   def set_default_status
     self.status = :requested if status.blank?
+  end
+  def start_date_before_end_date
+    if start_date.present? && end_date.present? && start_date > end_date
+      errors.add(:start_date, "must be before or equal to end date")
+    end
   end
 end
