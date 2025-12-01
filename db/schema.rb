ffc8e743bd01a4afe80ef15d9125815f0b4b7aeb
lcm9995 <lcm9995@nyu.edu>
2025-12-01 10:32:35 -0500

    update db/schema ?

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_07_034231) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "account_status", default: "active", null: false
+    t.string "account_status"
     t.datetime "created_at", null: false
     t.string "email"
     t.string "password_digest"
