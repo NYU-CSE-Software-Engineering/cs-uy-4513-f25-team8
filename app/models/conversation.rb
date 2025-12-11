ecba20e9d39c9cf9c5ec302b0a722dfd5a385076
Jing Qian <jqian2272@gmail.com>
2025-12-11 07:44:06 -0500

    add validations to conversation/message models

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,19 @@
 class Conversation < ApplicationRecord
   belongs_to :booking
-  belongs_to :owner
-  belongs_to :renter
+  belongs_to :owner,  class_name: "User"
+  belongs_to :renter, class_name: "User"
+
+  has_many :messages, dependent: :destroy
+
+  validates :booking, :owner, :renter, presence: true
+  validates :booking_id, uniqueness: { scope: [:owner_id, :renter_id] }
+  validate :owner_and_renter_must_be_different
+
+  private
+
+  def owner_and_renter_must_be_different
+    return if owner != renter
+
+    errors.add(:renter, 'must be different from owner')
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :conversation
-  belongs_to :sender
+  belongs_to :sender, class_name: "User"
+
+  validates :body, presence: true
+  validate :sender_must_be_participant
+
+  private
+
+  def sender_must_be_participant
+    return if conversation.nil?
+
+    unless [conversation.owner, conversation.renter].include?(sender)
+      errors.add(:sender, "must be a participant in the conversation")
+    end
+  end
 end
